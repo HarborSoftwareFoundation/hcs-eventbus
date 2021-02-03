07416ebb994027bffecbf6a6f32f6fad0e886417
Alexander Bocharov <alex.bocharov@outlook.com>
2021-02-03 10:42:51 +0300

    Update license information

--- src/src/Pcs.EventBus.Abstractions/IntegrationEvent.cs
+++ src/src/Pcs.EventBus.Abstractions/IntegrationEvent.cs
@@ -1,4 +1,7 @@
-﻿namespace Parusnik.Pcs.EventBus
+﻿// Copyright (c) Parusnik.
+// Licensed under the MIT License. See the LICENSE file in the project root for more information.
+
+namespace Parusnik.Pcs.EventBus
 {
     public record IntegrationEvent
     {
